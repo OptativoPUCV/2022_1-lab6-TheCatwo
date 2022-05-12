7c195a1f91d34532078bfa8fddaf3cdd5947b2ef
CRISTOBALRODRI2 <>
2022-05-12 16:36:26 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -56,7 +56,7 @@ List* get_adj_nodes(Node* n){
  
     for(a=0; a<9; a++){
       for(b=0; b<9; b++){
-        if(!i->sudo){
+        if(i->sudo==NULL){
           i->sudo[a][b]=cont;
           cont++;
         }  
